7c1282f69db1305fe86406bbdeaff5a93ff9f17c
Muhammad Furqan Asif <45694268+mfurqanasif@users.noreply.github.com>
2024-05-31 15:27:35 +0500

    Update axios.js
    update ssl

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const server = 'https://16.170.210.194:3000/api';
+const server = 'https://51.20.176.204:443/api';
 
 const instance = axios.create({
   baseURL: server,
