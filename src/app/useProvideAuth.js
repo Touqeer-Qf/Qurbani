0be6b91653ac26809fb6e6cfa38673d680797447
Touqeer-Qf <136805057+Touqeer-Qf@users.noreply.github.com>
2024-06-05 15:08:52 +0500

    Update useProvideAuth.js

--- src/app/useProvideAuth.js
+++ src/app/useProvideAuth.js
@@ -3,15 +3,20 @@ import { useNavigate } from 'react-router-dom';
 
 const useProvideAuth = () => {
   const [user, setUser] = useState(localStorage.getItem('token'));
+  const [name, setName] = useState(localStorage.getItem('name'));
 
   const login = (data) => {
     setUser(data.token);
+    setName(data.name);
     localStorage.setItem('token', data.token);
+    localStorage.setItem('name', data.name);
   };
 
   const logout = () => {
     setUser(null);
+    setName(null);
     localStorage.removeItem('token');
+    localStorage.removeItem('name');
   };
 
   const verifyToken = () => {
@@ -19,7 +24,7 @@ const useProvideAuth = () => {
     setUser(token)
   };
 
-  return { user, setUser, login, logout, verifyToken };
+  return { user, name, setUser, login, logout, verifyToken };
 };
 
 export default useProvideAuth;
